7fb1da05e79124a56a4e6f92a44748f825e9ba65
Alex <alexnobrecruz@yahoo.com.br>
2024-09-10 06:34:48 -0300

    Primeiro commit e inicialização do repositorio

---
+++ README.md
@@ -0,0 +1,33 @@
+<h1 align="center"> Intro HMTML e JS </h1>
+
+<p align="center">
+Projeto simples. 
+<br>
+Possui um html onde é carregado um arquivo JS com alguns operações. 
+<br>
+O código no arquivo  JS está comentadado, para execução do mesmo e visualizar o resultado, descomente a parte que for executar.
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e JavaScript
+- Git e Github
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT.
+
+---
---
+++ main.js
@@ -0,0 +1,76 @@
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador. 
+//alert('Hello world');
+//console.log('Hello world');
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+// let valor1, valor2;
+// valor1 = 10;
+// valor2 = 20;
+// console.log(valor1 + valor2);
+
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+// let input1;
+// input1 = prompt('Informe um número');
+// if(typeof input1 == Number){
+//     console.log('É um número.')
+// }else{
+//     console.log('Não é número.')
+// }
+
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+// let input2;
+// input2 = Number(prompt('Informe um texto'));
+// if(isNaN(input2)){
+//     console.log('É um texto.')
+// }else{
+//     console.log('Não é texto.')
+// }
+
+// 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+// let boo;
+// boo = true;
+// if(typeof boo === "boolean"){
+//     console.log('É um Boolean.')
+// }else{
+//     console.log('Não é Boolean.')
+// }
+
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+// let valor1, valor2;
+// valor1 = Number(200);
+// valor2 = Number(30);
+// console.log(valor1 - valor2);
+
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+// let valor1, valor2;
+// valor1 = Number(100);
+// valor2 = Number(30);
+// console.log(valor1 * valor2);
+
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+// let valor1, valor2;
+// valor1 = Number(100);
+// valor2 = Number(5);
+// if(valor2 > 0){
+//     console.log(valor1 / valor2);
+// }else{
+//     console.log('Não é possível divisão por 0 (zero).')
+// }
+
+// 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+// let valor1;
+// valor1 = Number(100);
+// if(valor1 % 2 == 0){
+//     console.log(`O número ${valor1} é par.`)
+// }else{
+//     console.log(`O número ${valor1} não é par.`)
+// }
+
+// 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+// let valor1;
+// valor1 = Number(101);
+// if(valor1 % 2 != 0){
+//     console.log(`O número ${valor1} é ímpar.`)
+// }else{
+//     console.log(`O número ${valor1} não é ímpar.`)
+// }
